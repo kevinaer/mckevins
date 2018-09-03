f438346f04022737102334236b9337081578091e
Kevin Escobar Rodriguez <kevinaer@mit.edu>
2018-09-02 20:14:05 -0400

    Prevent user w/o id to be created

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -5,27 +5,32 @@ const Cookies = require('universal-cookie');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    User.findById(req.body.id, (err, query) => {
-        const cookies = new Cookies();
-        if (query) {
-            cookies.set('name', 'query', { path: '/', maxAge: 86400 });
-            res.send(query);
-        } else {
-            User.create({
-                name: req.body.name,
-                _id: req.body.id,
-                url: req.body.url || `https://graph.facebook.com/${req.body.id}/picture?type=large`,
-                accessToken: req.body.accessToken,
-            }, (createErr, object) => {
-                if (object) {
-                    cookies.set('name', 'query', { path: '/', maxAge: 86400 });
-                    res.send(object);
-                } else {
-                    res.status(404).send('Cannot retrieve user');
-                }
-            });
-        }
-    });
+    if (!req.body.id) {
+        res.status(404).send('Cannot create user');
+    } else {
+        User.findById(req.body.id, (err, query) => {
+            const cookies = new Cookies();
+            if (query) {
+                cookies.set('name', 'query', { path: '/', maxAge: 86400 });
+                res.send(query);
+            } else {
+                User.create({
+                    name: req.body.name,
+                    _id: req.body.id,
+                    url: req.body.url || `https://graph.facebook.com/${req.body.id}/picture?type=large`,
+                    accessToken: req.body.accessToken,
+                }, (createErr, object) => {
+                    if (object) {
+                        cookies.set('name', 'query', { path: '/', maxAge: 86400 });
+                        res.send(object);
+                    } else {
+                        res.status(404).send('Cannot retrieve user');
+                    }
+                });
+            }
+        });
+    }
+    
 });
 
 router.put('/user/admin', (req, res) => {
